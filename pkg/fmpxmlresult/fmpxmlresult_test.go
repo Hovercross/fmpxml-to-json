5536dc384edec8b98d924880f7d14aef203563dc
Adam Peacock <adam@thepeacock.net>
2021-06-27 09:11:52 -0400

    Add some tests for a variety of edge cases

--- pkg/fmpxmlresult/fmpxmlresult_test.go
+++ pkg/fmpxmlresult/fmpxmlresult_test.go
@@ -17,6 +17,7 @@ func Test_Populate(t *testing.T) {
 			{EmptyOK: true, MaxRepeat: 1, Name: "Birthday", Type: "DATE"},
 			{EmptyOK: true, MaxRepeat: 1, Name: "Favorite Time", Type: "TIME"},
 			{EmptyOK: true, MaxRepeat: 2, Name: "Favorite Number", Type: "NUMBER"},
+			{EmptyOK: true, MaxRepeat: 1, Name: "Favorite Pie", Type: "TEXT"},
 		},
 	}
 
@@ -38,6 +39,7 @@ func Test_Populate(t *testing.T) {
 				{Data: []string{"1/11/1986"}},
 				{Data: []string{"8:09:21 PM"}},
 				{Data: []string{"42", "41.1"}},
+				{Data: []string{}},
 			}},
 		},
 	}
@@ -54,6 +56,7 @@ func Test_Populate(t *testing.T) {
 
 	if err := sample.PopulateRecords(); err != nil {
 		t.Error(err)
+		return
 	}
 
 	// Records should be quoted as raw messages
@@ -65,6 +68,7 @@ func Test_Populate(t *testing.T) {
 			"Birthday":        json.RawMessage(`"1986-01-11"`),
 			"Favorite Time":   json.RawMessage(`"20:09:21"`),
 			"Favorite Number": json.RawMessage(`[42,41.1]`),
+			"Favorite Pie":    json.RawMessage("null"),
 
 			"recordID":       json.RawMessage(`"683"`),
 			"modificationID": json.RawMessage(`"196"`),
@@ -73,5 +77,153 @@ func Test_Populate(t *testing.T) {
 
 	for _, diff := range deep.Equal(sample.Records, expectedRecords) {
 		t.Error(diff)
+	}
+}
+
+func Test_InvalidNumber(t *testing.T) {
+	metadata := fmpxmlresult.Metadata{
+		Fields: []fmpxmlresult.Field{
+			{EmptyOK: true, MaxRepeat: 1, Name: "First", Type: "NUMBER"},
+		},
+	}
+
+	database := fmpxmlresult.Database{
+		DateFormat: "M/d/yyyy",
+		Layout:     "Overview",
+		Name:       "test.fmp12",
+		Records:    1,
+		TimeFormat: "h:mm:ss a",
+	}
+
+	resultSet := fmpxmlresult.ResultSet{
+		Found: 1,
+		Rows: []fmpxmlresult.Row{
+			{ModID: "196", RecordID: "683", Cols: []fmpxmlresult.Col{
+				{Data: []string{"Adam"}},
+			}},
+		},
+	}
+
+	sample := fmpxmlresult.FMPXMLResult{
+		ErrorCode: 15,
+		Database:  database,
+		Metadata:  metadata,
+		ResultSet: resultSet,
+	}
+
+	if err := sample.PopulateRecords(); err == nil {
+		t.Error("Err is nil")
+
+	}
+}
+
+func Test_InvalidDate(t *testing.T) {
+	metadata := fmpxmlresult.Metadata{
+		Fields: []fmpxmlresult.Field{
+			{EmptyOK: true, MaxRepeat: 1, Name: "First", Type: "DATE"},
+		},
+	}
+
+	database := fmpxmlresult.Database{
+		DateFormat: "M/d/yyyy",
+		Layout:     "Overview",
+		Name:       "test.fmp12",
+		Records:    1,
+		TimeFormat: "h:mm:ss a",
+	}
+
+	resultSet := fmpxmlresult.ResultSet{
+		Found: 1,
+		Rows: []fmpxmlresult.Row{
+			{ModID: "196", RecordID: "683", Cols: []fmpxmlresult.Col{
+				{Data: []string{"Adam"}},
+			}},
+		},
+	}
+
+	sample := fmpxmlresult.FMPXMLResult{
+		ErrorCode: 15,
+		Database:  database,
+		Metadata:  metadata,
+		ResultSet: resultSet,
+	}
+
+	if err := sample.PopulateRecords(); err == nil {
+		t.Error("Err is nil")
+
+	}
+}
+
+func Test_TooManyScalars(t *testing.T) {
+	metadata := fmpxmlresult.Metadata{
+		Fields: []fmpxmlresult.Field{
+			{EmptyOK: true, MaxRepeat: 1, Name: "First", Type: "NUMBER"},
+		},
+	}
+
+	database := fmpxmlresult.Database{
+		DateFormat: "M/d/yyyy",
+		Layout:     "Overview",
+		Name:       "test.fmp12",
+		Records:    1,
+		TimeFormat: "h:mm:ss a",
+	}
+
+	resultSet := fmpxmlresult.ResultSet{
+		Found: 1,
+		Rows: []fmpxmlresult.Row{
+			{ModID: "196", RecordID: "683", Cols: []fmpxmlresult.Col{
+				{Data: []string{"42", "43", "44"}},
+			}},
+		},
+	}
+
+	sample := fmpxmlresult.FMPXMLResult{
+		ErrorCode: 15,
+		Database:  database,
+		Metadata:  metadata,
+		ResultSet: resultSet,
+	}
+
+	if err := sample.PopulateRecords(); err == nil {
+		t.Error("Err is nil")
+
+	}
+}
+
+func Test_InvalidArray(t *testing.T) {
+	metadata := fmpxmlresult.Metadata{
+		Fields: []fmpxmlresult.Field{
+			{EmptyOK: true, MaxRepeat: 2, Name: "First", Type: "NUMBER"},
+		},
+	}
+
+	database := fmpxmlresult.Database{
+		DateFormat: "M/d/yyyy",
+		Layout:     "Overview",
+		Name:       "test.fmp12",
+		Records:    1,
+		TimeFormat: "h:mm:ss a",
+	}
+
+	resultSet := fmpxmlresult.ResultSet{
+		Found: 1,
+		Rows: []fmpxmlresult.Row{
+			{ModID: "196", RecordID: "683", Cols: []fmpxmlresult.Col{
+				{Data: []string{"Adam"}},
+			}},
+		},
+	}
+
+	sample := fmpxmlresult.FMPXMLResult{
+		ErrorCode: 15,
+		Database:  database,
+		Metadata:  metadata,
+		ResultSet: resultSet,
+	}
+
+	if err := sample.PopulateRecords(); err == nil {
+		t.Error("Err is nil")
+
 	}
 }
